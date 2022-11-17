ab8665f8418102265e5e15922ec5d2efc8b32090
Jonathan Luo <97814319+zombie0000@users.noreply.github.com>
2022-11-17 14:44:02 -0700

    sagdsfg

--- main.js
+++ main.js
@@ -6,9 +6,23 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 function btnClicked() {
   //Input
   let name = document.getElementById("char-in").value;
-
+  console.log(name);
   // If Statement - Test the Input
   if (name === "Po") {
-    console.log("Yes, Po is a KF Panfa Character");
+    document.getElementById("char-name").innerHTML = "Po";
+    document.getElementById("char-quote").innerHTML =
+      "Buddy, I am the Dragon Warrior!";
+    document.getElementById("char-img").src = "img/po.png";
+  } else if (name === "Tigress") {
+    document.getElementById("char-name").innerHTML = "Tigress";
+    document.getElementById("char-quote").innerHTML = "That was hardcore!";
+    document.getElementById("char-img").src = "img/tigress.png";
+  } else if (name === "Mantis") {
+    console.log("Yes, Mantis is a KF Panda character");
+  } else if (name === "Monkey") {
+    console.log("Yes, Monkey is a KF Panda character");
+  } else {
+    document.getElementById("char-name").innerHTML = "?????";
+    document.getElementById("char-quote");
   }
 }
